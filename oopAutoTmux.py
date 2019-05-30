071f59823a8eeffa9f3feaa4de455bfefc4ef3fd
tenordiamond <christer.hellkvist@gmail.com>
2019-05-30 10:12:36 +0200

    Update oopAutoTmux.py

--- oopAutoTmux.py
+++ oopAutoTmux.py
@@ -1,19 +1,28 @@
 '''
-Automatic start of tmux with separate windows for each basic enumeration tool. Switching to nmap window and starts a initial scan.
+Automatic start of tmux with separate windows for each basic enumeration tool of your choise. Switching to nmap window and starts a initial scan on the provided ipadress.
 '''
 import sys
 import os
+import argparse
+
+parser = argparse.ArgumentParser(description = "Tmux startscript")
+parser.add_argument('-w', '--windows', type = str, metavar='', required=True, help='A comma separated list of window names you want ex: "window1,window2,window3"')
+parser.add_argument('-i', '--ipadress', type = str, metavar='', required=True, help='Target ipadress')
+parser.add_argument('-n', '--session_name', type = str, metavar='', required=True, help='The tmux session name')
+args = parser.parse_args()
+
 
 class tmux:
-    def __init__(self, name):
+    def __init__(self, name, windows):
         self.name = name
+        self.windows = windows
     
     
     def create_session(self):
         return os.system('tmux -2 new-session -d -s %s'%(self.name))
     
-    def create_std_windows(self):
-        app_list = ['nmap','nikto','gobuster']
+    def create_windows(self):
+        app_list = [str(app) for app in args.windows.split(',')]
         for app in app_list:
             os.system('tmux new-window -t %s:1 -n %s'%(self.name, app))
 
@@ -22,19 +31,28 @@ class tmux:
         os.system('tmux select-window -t %s:1'%(self.name))
         os.system('tmux -2 attach-session -t %s'%(self.name))
 
+
+    #def start_nmap(self):
+    #    app_list = [str(app) for app in args.windows.split(',')]
+    #    index = 0
+    #    for app in app_list:
+    #        if app == 'nmap':
+    #            #os.system('tmux select-window -t %:%'%(self.name,index))
+    #            #os.system('tmux send-keys "nmap -sV -sC -oA %s %s" C-m'%(self.name, self.t_adress))
+    #        else:
+    #            index += 1
+
 class target:
-    def __init__(self, t_name, t_domain, full_target):
-        self.t_name = t_name
-        self.t_domain = t_domain
-        self.f_target = full_target
+    def __init__(self, t_ipadress):
+        self.t_ipadress = t_ipadress
 
+    
+def main():
+    t = target(args.ipadress)
+    tmux_object = tmux(args.session_name, args.windows)
+    tmux_object.create_session()
+    tmux_object.create_windows()
+    tmux_object.attach_session()
 
-#t = target(sys.argv[1], sys.argv[2], sys.argv[1] + "." + sys.argv[2])
-#x = tmux(t.t_name)
-#x.create_session()
-#x.create_std_windows()
-#x.attach_session()
-
-#starting nmap needs to get a method!!
-#os.system('tmux send-keys "nmap -sV -sC -oA %s %s" C-m'%(target_name,full_target))
-
+if __name__=='__main__':
+    main()
